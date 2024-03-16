7f49b297f438ecded33523419a2db70f2b2a3768
hellboy2364 <160237839+hellboy2364@users.noreply.github.com>
2024-03-16 14:55:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,55 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
+        }
+    }
+    return count;
+    return 0;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = len - 1; j > i; j--) {
+            if (arr[i] + arr[j] > value) {
+                continue;
+            }
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
+        }
+    }
+    return count;
+    return 0;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len - 1; i++) {
+        int x = bin_search(arr,i + 1, len - 1, value - arr[i]);
+        int y = x;
+        while (arr[y] == value - arr[i] && y > i) {
+            count++;
+            y--;
+        }
+        while (arr[x + 1] == value - arr[i]) {
+            count++;
+            x++;
+        }
+    }
+    return count;
+    return 0;
+}
+int bin_search(int *A, int left, int right, int x) {
+    int mid = (left + right) / 2;
+    if (left == mid)
+        return A[left] == x ? left : A[right] == x ? right : -left;
+    if (A[mid] < x)
+        left = mid;
+    else
+        right = mid;
+    return bin_search(A, left, right, x);
 }
